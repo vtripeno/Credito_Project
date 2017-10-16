7fe39e4846b0e810295d4cd81508d98b748825ef
victor <vtripeno@gmail.com>
2017-10-15 23:45:06 -0200

    Ajuste layout

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,3 +1,46 @@
+//Verifica se CPF é válido
+function testaCPF(strCPF) {
+    var soma, resto;
+    soma = 0;
+
+    if (strCPF == "00000000000"){
+        document.getElementById("cpf").setCustomValidity('Invalid');
+        return false;
+    }
+
+    for (i=1; i<=9; i++){
+        soma = soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if ((resto == 10) || (resto == 11)){
+        resto = 0;
+    }
+
+    if (resto != parseInt(strCPF.substring(9, 10))){
+        document.getElementById("cpf").setCustomValidity('Invalid');
+        return false;
+    }
+
+    soma = 0;
+    for (i = 1; i <= 10; i++){
+        soma = soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if ((resto == 10) || (resto == 11)){
+        resto = 0;
+    }
+
+    if (resto != parseInt(strCPF.substring(10, 11))){
+        document.getElementById("cpf").setCustomValidity('Invalid');
+        return false;
+    }
+
+    document.getElementById("cpf").setCustomValidity('');
+    return true;
+}
+
 function httpGet(theUrl) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
